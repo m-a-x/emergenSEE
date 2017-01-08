f94b8b17f8db7e07b5227c9ffd4eb459f0ef2db3
Max Mattioli <max@l2inc.com>
2017-01-08 18:20:09 -0500

    setting up

--- setup.py
+++ setup.py
@@ -1,11 +1,4 @@
 from setuptools import setup, find_packages
-import os
-
-
-REQUIREMENTS = [
-    line.strip() for line in open(os.path.join(os.path.dirname(__file__),
-                                               'requirements.txt')).readlines()]
-
 setup(
   name = 'emergenSEE',
   packages = ['emergenSEE'], # this must be the same as the name above
@@ -17,20 +10,13 @@ setup(
   url = 'https://github.com/m-a-x/emergenSEE', # use the URL to the github repo
   download_url = 'https://github.com/m-a-x/emergenSEE/tarball/0.1',
   license='MIT',
-  classifiers = [
-        "Programming Language :: Python :: 2.7",
-        "Development Status :: 3 - Alpha",
-        "Environment :: Other Environment",
-        "Operating System :: OS Independent",
-        'License :: OSI Approved :: MIT License',
-        "Topic :: Software Development :: Libraries :: Python Modules"
-  ],
+  classifiers = [],
   install_requires=['imageio==1.6',
                     'pandas',
                     'numpy',
                     'matplotlib',
                     'descartes',
                     'shapely',
-                    'scikit-learn',
+                    'scikit-learn'
                       ],
 )
