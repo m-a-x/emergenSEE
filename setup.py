e677298edbb4b829e6c17632287b49dece456b51
Max Mattioli <max@l2inc.com>
2017-01-08 19:32:43 -0500

    tryna pypi

--- setup.py
+++ setup.py
@@ -10,5 +10,7 @@ setup(
   url = 'https://github.com/m-a-x/emergenSEE', # use the URL to the github repo
   download_url = 'https://github.com/m-a-x/emergenSEE/tarball/0.5',
   license='MIT',
+  include_package_data=True,
+  install_requires=['pandas>=0.19.1', 'numpy>=1.11.3', 'matplotlib>=1.5.3', 'descartes>=1.0.2', 'scikit-learn>=0.18.1', 'imageio==1.6'],
   classifiers = []
 )
