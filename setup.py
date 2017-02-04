6b21564a30e2bc67495a5f3bfa3ae6f95f2e3f1c
Max Mattioli <max@l2inc.com>
2017-02-03 22:08:55 -0500

    still tryna peeeep

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Max Mattioli',
   author_email = 'm.max@columbia.edu',
   url = 'https://github.com/m-a-x/emergenSEE',
-  download_url = 'https://github.com/m-a-x/emergenSEE/tarball/0.9.1',
+  download_url = 'https://github.com/m-a-x/emergenSEE/tarball/0.9.3',
   license='MIT',
   install_requires=['pandas>=0.19.1', 'numpy>=1.11.3', 'matplotlib>=1.5.3', 'descartes>=1.0.2', 'scikit-learn>=0.18.1', 'imageio==1.6'],
   package_data={'emergenSEE': ['data/*.pkl']},
